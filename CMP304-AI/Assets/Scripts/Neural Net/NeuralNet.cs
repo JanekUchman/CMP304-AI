891fc2bb160d5e59f023a0af7e26e795d3cc52fb
JanekUchman <janekuchman@hotmail.co.uk>
2019-02-13 18:25:51 +0000

    Starting to build Neural Net. Set each node to have a series of inputs. Output can be calculated from input nodes. Layers are cycled through and calculated one at a time. Output is returned as a list. Different activation functions can be used. Connections and layers are set in the Unity editor.

---
+++ CMP304-AI/Assets/Scripts/Neural Net/NeuralNet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuralNet : MonoBehaviour {
+
+	[SerializeField] private NeuralNode.ActivationFunctionTypes activationFunctionType = NeuralNode.ActivationFunctionTypes.HYPERBOLIC_TANGENT;
+	[SerializeField] private NeuralLayer[] layers;
+
+	const int inputLayer = 0;
+
+	public List<float> GetNeuralOutput(float[] inputs)
+	{
+		if (inputs.Length != layers[inputLayer].nodes.Length) Debug.LogError("Neural Net: Input node mismatch, check input layers and input values.");
+		for (int i = 0; i < inputs.Length; i++)
+		{
+			layers[inputLayer].nodes[i].SetInputNodeValue(inputs[i]);
+		}
+		
+		for (int i = 1; i < layers.Length; i ++) 
+		{
+			foreach (var node in layers[i].nodes)
+			{
+				node.SetOutput(activationFunctionType);
+			}
+		}
+
+		List<float> outputValues = new List<float>();
+		foreach (var outputLayerNode in layers[layers.Length-1].nodes)
+		{
+			outputValues.Add(outputLayerNode.Output);
+		}
+
+		return outputValues;
+	}
+	
+	[System.Serializable]
+	public class NeuralLayer
+	{
+		public NeuralNode[] nodes;
+	}
+}
---
+++ CMP304-AI/Assets/Scripts/Neural Net/NeuralNode.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeuralNode : MonoBehaviour
+{
+	public enum ActivationFunctionTypes
+	{
+		LOGISTIC_SIGMOID,
+		HYPERBOLIC_TANGENT
+	}
+
+	//Set in inspector
+	[SerializeField] private NeuralNodeConnection[] inputConnections;
+	public float weighting;
+	private List<float> outputValues = new List<float>();
+	
+	private float output;
+
+	public float Output
+	{
+		get { return output; }
+	}
+
+	public void SetOutput(ActivationFunctionTypes activationFunctionUsed)
+	{
+		foreach (var nodeConnection in inputConnections)
+		{
+			SumInput(nodeConnection);
+		}
+		ApplyActivationFunction(activationFunctionUsed);
+	}
+
+	//Use for the first layer of nodes
+	public void SetInputNodeValue(float output)
+	{
+		this.output = output;
+	}
+
+	private void SumInput(NeuralNodeConnection nodeConnection)
+	{
+		output += nodeConnection.inputNode.output * nodeConnection.weighting;
+	}
+
+	private void ApplyActivationFunction(ActivationFunctionTypes activationFunctionUsed)
+	{
+		switch (activationFunctionUsed)
+		{
+			case ActivationFunctionTypes.LOGISTIC_SIGMOID:
+				output= Sigmoid(output);
+				break;
+			case ActivationFunctionTypes.HYPERBOLIC_TANGENT:
+				output= TanH(output);
+				break;
+		}
+	}
+
+	private float Sigmoid(float outputValue)
+	{
+		if (outputValue > 10) return 1.0f;
+		else if (outputValue < -10) return 0.0f;
+		else return (float)(1.0 / (1.0 + Math.Exp(-outputValue)));
+	}
+
+	private float TanH(float outputValue)
+	{
+		if (outputValue > 10) return 1.0f;
+		else if (outputValue < -10) return 0.0f;
+		else return (float)Math.Tanh(outputValue);
+	}
+
+	[Serializable]
+	public class NeuralNodeConnection
+	{
+		public float weighting;
+		public NeuralNode inputNode;
+	}
+}
